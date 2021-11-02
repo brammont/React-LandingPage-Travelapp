a5701ce6811cda35b549e14edec52a63ebc12c85
Brammont <brayanmazuera1@gmail.com>
2021-11-02 11:32:20 -0500

    configurar el setState para el cambio de logo en el menu de la navegacion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 function Navbar() {
+    const [click,setClick] = useState{false};
     return (
         <>
             <nav className="navbar">
@@ -8,6 +9,9 @@ function Navbar() {
                     <Link to="/" className="navbar-logo">
                         TRVL <i className="fab fa-typo3"/>
                     </Link>
+                    <div className="manu-icon">
+                        <i className={click ? 'fas fa-times': 'fas fa-bars'}/>
+                    </div>
                 </div>
             </nav>
         </>

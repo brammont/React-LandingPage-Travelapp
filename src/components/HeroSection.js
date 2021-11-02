12a419499ce558aea3d4fc8eae609b38fc06845e
Brammont <brayanmazuera1@gmail.com>
2021-11-02 18:09:29 -0500

    crear compoenente hero principal

---
+++ src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Button } from './Button'
+
+function HeroSection() {
+    return (
+        <div className="hero-container">
+            <video src="/videos/video-2.mp4" autoPlay loop muted/>
+            <h1>Las aventuras te aguardan...</h1>
+            <p>¿Que estas esperando?</p>
+            <div className="hero-btns">
+                <Button 
+                    className="btns" 
+                    buttonStyle="btn--outline" 
+                    buttonSize="btn--large"
+                >
+                    Empezar
+                </Button> 
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection

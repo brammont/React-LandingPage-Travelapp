c555c66c0a344260477c72af5072816f09bc7d39
Brammont <brayanmazuera1@gmail.com>
2021-11-02 11:25:04 -0500

    configurarn la libreria react-dom-ruter con  el componente nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        TRVL <i className="fab fa-typo3"/>
+                    </Link>
+                </div>
+            </nav>
+        </>
     )
 }
 

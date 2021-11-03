6fa1b65c58d290f939a95a73112c29dd08cb38c0
Brammont <brayanmazuera1@gmail.com>
2021-11-03 09:36:01 -0500

    paginas con sus respectivos links en la app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
+import Servicios from './components/pages/Servicios';
+import Productos from './components/pages/Productos';
+import Registro from './components/pages/Registro';
 function App() {
   return (
     <>
@@ -9,6 +12,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/services" exact component={Servicios} />
+          <Route path="/products" exact component={Productos} />
+          <Route path="/Sing-up" exact component={Registro} />
         </Switch>
       </Router>
     </>
